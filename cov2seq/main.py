3d61c0d738e94aee69de45cf01dc9d6ac0af8bf7
Markus Haak <mhaak@techfak.uni-bielefeld.de>
2021-01-04 17:35:17 +0100

    updated ncov submodule

---
+++ cov2seq/main.py
@@ -0,0 +1,26 @@
+import os
+from Bio import SeqIO
+import vcf
+import numpy as np
+import subprocess
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.colors as colors
+from operator import itemgetter
+from collections.abc import Iterable
+import matplotlib.gridspec as gridspec
+import json
+import matplotlib.patches as mpatch
+from datetime import datetime
+
+def update():
+	print("update")
+
+def report():
+	print("report")
+
+if __name__ == '__main__':
+	print('main')
