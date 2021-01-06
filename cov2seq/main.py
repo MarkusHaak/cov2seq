2acf0d895acca7ef2415b59c60c06faacf4314e8
Markus Haak <mhaak@techfak.uni-bielefeld.de>
2021-01-06 12:49:28 +0100

    adds command-line argument parser and config file parser

--- cov2seq/main.py
+++ cov2seq/main.py
@@ -1,27 +1,157 @@
 import os
-from Bio import SeqIO
-import vcf
-import numpy as np
-import subprocess
-import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.colors import LogNorm
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-import matplotlib.colors as colors
-from operator import itemgetter
-from collections.abc import Iterable
-import matplotlib.gridspec as gridspec
-import json
-import matplotlib.patches as mpatch
-from datetime import datetime
+import sys
+import configparser
+import logging
+import pathlib
+import pkg_resources
+import argparse
 
-def update():
-	print("update")
+class ArgHelpFormatter(argparse.HelpFormatter):
+    '''
+    Formatter adding default values to help texts.
+    '''
+    def __init__(self, prog):
+        super().__init__(prog)
 
-def report():
-	print("report")
+    def _get_help_string(self, action):
+        text = action.help
+        if action.default is not None and \
+           action.default != argparse.SUPPRESS and \
+           '(default' not in text.lower():
+           text += ' ({})'.format(action.default)
+        return text
+
+def get_package_info():
+    pkg_dir = os.path.dirname(pathlib.Path(__file__).parent.absolute())
+    pkg_res = pkg_resources.get_distribution('cov2seq')
+    pkg_version = pkg_res.version
+    pkg_descr = list(pkg_res._get_metadata(pkg_res.PKG_INFO))[-2]
+    return pkg_dir, pkg_version, pkg_descr
+
+def read_configuration(argv, pkg_dir, defaults=None):
+    def update_defaults(conf_file):
+        config = configparser.SafeConfigParser()
+        config.read([conf_file])
+        defaults.update(dict(config.items("DEFAULT")))
+
+    if defaults is None:
+        defaults = {}
+
+    default_cfg = os.path.join(pkg_dir, "cov2seq", "cov2seq.cfg")
+    user_cfg = os.path.join(os.path.expanduser('~'), "cov2seq.cfg")
+    local_cfg = "cov2seq.cfg"
+
+    conf_parser = argparse.ArgumentParser(
+        description=__doc__,        
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        add_help=False)
+    conf_parser.add_argument("-c", "--conf_file",
+        help='''Specify an additional config file location with highest priority. 
+             (defaults, increasing priority: {})'''.format([default_cfg, user_cfg, local_cfg]), 
+        metavar="FILE")
+    args, remaining_argv = conf_parser.parse_known_args()
+
+    if os.path.exists(default_cfg):
+        update_defaults(default_cfg)
+    else:
+        logging.warning("Package-default configuration file not found.")
+    if os.path.exists(user_cfg):
+        update_defaults(user_cfg)
+    else:
+        logging.debug("No user configuration file found at {}.".format(user_cfg))
+    if os.path.exists(local_cfg):
+        update_defaults(local_cfg)
+    else:
+        logging.debug("No local configuration file {} found.".format(local_cfg))
+    if args.conf_file:
+        update_defaults(args.config_file)
+
+    return conf_parser, remaining_argv, defaults
+
+def add_main_group_to_parser(parser):
+    main_group = parser.add_argument_group('Main Options')
+    main_group.add_argument('--nanopore_dir',
+        help='''base directory containing sample-named sub-directories of nanopore datasets analyzed
+             with the artic-ncov2019 pipeline. The directory tree must be 
+             <nanopore_dir>/<sample_name>/artic-medaka/<scheme_name>/<scheme_version>/<sample_name>.*''')
+    main_group.add_argument('--illumina_dir',
+        help='''base directory containing sample-named sub-directories of illumina datasets. 
+             The directory tree must be <illumina_dir>/<sample_name>/*.bam''')
+    main_group.add_argument('--sanger_dir',
+        help='''base directory containing sample-named sub-directories of sanger data. 
+             The directory tree must be <sanger_dir>/<sample_name>/*-<primer_id>.ab1''')
+    main_group.add_argument('--reference',
+        help='Fasta file containing the reference sequence used in the artic pipeline.')
+    main_group.add_argument('--reference_annotation',
+        help='.json file with basic gene annotation information.')
+    main_group.add_argument('--primer_schemes_dir',
+        help='''Direcotry containing the primer scheme files in a directory tree as follows:
+             <primer_schemes_dir>/<scheme_name>/<scheme_version>/<scheme_name>.*''')
+    main_group.add_argument('--results_dir',
+        help='''Directory for saving sample results.''')
+    main_group.add_argument('--nextstrain_ncov',
+        help='''Path to the directory containing the Nextstrain ncov git repository. This should
+             be updated regularly for accurate clade assignment.''')
+    return parser
+
+def add_help_group_to_parser(parser):
+    help_group = parser.add_argument_group('Help')
+    help_group.add_argument('--version', 
+        action='version',
+        default=argparse.SUPPRESS,
+        version="cov2seq {}".format(pkg_version))
+    help_group.add_argument('-h', '--help', 
+        action='help', 
+        default=argparse.SUPPRESS,
+        help='Show this help message and exit.')
+    return parser
+
+def init_parser(argv, defaults, script_descr=""):
+    pkg_dir, pkg_version, pkg_descr = get_package_info()
+    
+    nextstrain_ncov = os.path.join(pkg_dir, 'ncov')
+    if os.path.exists(nextstrain_ncov):
+        defaults.update({'nextstrain_ncov' : nextstrain_ncov})
+
+    conf_parser, remaining_argv, defaults = read_configuration(argv, pkg_dir, defaults)
+
+    descr = pkg_descr
+    if script_descr:
+        descr += "\n" + script_descr
+
+    parser = argparse.ArgumentParser(
+        description=descr,
+        parents=[conf_parser],
+        formatter_class=ArgHelpFormatter, 
+        add_help=False)
+    parser.set_defaults(**defaults)
+
+    return add_main_group_to_parser(parser)
+
+def update(argv=None):
+    if argv is None:
+        argv = sys.argv
+    defaults = {} # configuration file independent default values; lowest priority
+    parser = init_parser(argv, defaults)
+
+
+
+    parser = add_help_group_to_parser(parser)
+    args = parser.parse_args(remaining_argv)
+
+def report(argv=None):
+    if argv is None:
+        argv = sys.argv
+    defaults = {} # configuration file independent default values; lowest priority
+    script_descr = "This script creates comprehensive reports for one or several samples."
+    parser = init_parser(argv, defaults, script_descr)
+
+    
+
+    parser = add_help_group_to_parser(parser)
+    args = parser.parse_args(remaining_argv)
 
 if __name__ == '__main__':
-	print('''This script is not intended for standalone command-line use. 
-		     Please install package and use cov2seq-update or cov2seq-report instead.''')
+    print('''This script is not intended for standalone command-line use. 
+             Please install package and use cov2seq-update or cov2seq-report instead.''')
+    exit(1)
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 import sys, os
 from setuptools import setup
+from glob import glob
 
 # https://stackoverflow.com/questions/36187264
 def binaries_directory():
@@ -24,54 +25,71 @@ def binaries_directory():
     print('no installation path found', file=sys.stderr)
     return None
 
+
+def submodule_data_files(submodule):
+    data_files = []
+    for dirpath, dirnames, filenames in os.walk(submodule):
+        if filenames:
+            install_dir = dirpath
+            list_entry = (install_dir, [os.path.join(dirpath, fn) for fn in filenames])
+            data_files.append(list_entry)
+    return data_files
+
 # ensure python version 3.7 or greater is used
 if (sys.version_info.major + .1 * sys.version_info.minor) < 3.7:
-	print('ERROR: please execute setup.py with python version >=3.7')
-	sys.exit(1)
+    print('ERROR: please execute setup.py with python version >=3.7')
+    sys.exit(1)
 
 # get version string from seperate file
 # https://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
 # https://stackoverflow.com/questions/436198/what-is-an-alternative-to-execfile-in-python-3/437857#437857
 VERSIONFILE="cov2seq/version.py"
 with open(VERSIONFILE) as f:
-	code = compile(f.read(), VERSIONFILE, 'exec')
-	exec(code)
+    code = compile(f.read(), VERSIONFILE, 'exec')
+    exec(code)
 if not __version__:
-	print("ERROR: unable to read version string from file {}".format(VERSIONFILE))
-	exit()
+    print("ERROR: unable to read version string from file {}".format(VERSIONFILE))
+    exit()
 
 DESCR = '''Comprehensive reports for Nanopore/Illumina/Sanger sequencing experiments of SARS CoV2 samples'''
 
 # load long description from Markdown file
 with open('README.md', 'rb') as readme:
-	LONG_DESCR = readme.read().decode()
+    LONG_DESCR = readme.read().decode()
+
+data_files = [('cov2seq', ['cfg/cov2seq.cfg'])]
+data_files.extend(submodule_data_files('.git'))
+data_files.extend(submodule_data_files('ncov'))
+
+for d, files in data_files:
+    print(d, files)
 
 setup(name='cov2seq',
-	  version=__version__,
-	  description=DESCR,
-	  long_description=LONG_DESCR,
-	  url='http://github.com/MarkusHaak/cov2seq',
-	  author='Markus Haak',
-	  author_email='markus.haak@posteo.net',
-	  license='GPL',
-	  packages=['cov2seq'],
-	  install_requires=['Biopython', 'pyvcf', 'numpy', 'pandas', 'matplotlib'],
-	  include_package_data=True,
-	  zip_safe=False,
-	  entry_points={"console_scripts": ['cov2seq-update = cov2seq.main:update',
-	  									'cov2seq-report = cov2seq.main:report']},
-	  data_files=[('cov2seq', ['cfg/cov2seq.cfg'])])
+      version=__version__,
+      description=DESCR,
+      long_description=LONG_DESCR,
+      url='http://github.com/MarkusHaak/cov2seq',
+      author='Markus Haak',
+      author_email='markus.haak@posteo.net',
+      license='GPL',
+      packages=['cov2seq'],
+      install_requires=['Biopython', 'pyvcf', 'numpy', 'pandas', 'matplotlib'],
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={"console_scripts": ['cov2seq-update = cov2seq.main:update',
+                                        'cov2seq-report = cov2seq.main:report']},
+      data_files=data_files)
 
 site_packages_dir = binaries_directory()
 if site_packages_dir:
-	pkg_dir = "cov2seq-{}-py{}.egg".format(__version__, sys.version_info.major + .1 * sys.version_info.minor)
-	default_cfg = os.path.join(site_packages_dir, pkg_dir, "cov2seq", "cov2seq.cfg")
-	user_cfg = os.path.join(os.path.expanduser('~'), "cov2seq.cfg")
-	print(default_cfg)
-	if os.path.exists(default_cfg):
-		print("\nINFO: default configuration file is found under {}".format(default_cfg))
-		print("If (partial) configurations are given in user config file {}, they are prioritized.".format(user_cfg))
-	else:
-		print("\nWARNING: default configuration file not found under {}".format(default_cfg))
-		print("Please supply all configurations found in configuration file \"{}\" in a user configuration file under {}".format("cov2seq.cfg", user_cfg))
+    pkg_dir = "cov2seq-{}-py{}.egg".format(__version__, sys.version_info.major + .1 * sys.version_info.minor)
+    default_cfg = os.path.join(site_packages_dir, pkg_dir, "cov2seq", "cov2seq.cfg")
+    user_cfg = os.path.join(os.path.expanduser('~'), "cov2seq.cfg")
+    print(default_cfg)
+    if os.path.exists(default_cfg):
+        print("\nINFO: default configuration file is found under {}".format(default_cfg))
+        print("If (partial) configurations are given in user config file {}, they are prioritized.".format(user_cfg))
+    else:
+        print("\nWARNING: default configuration file not found under {}".format(default_cfg))
+        print("Please supply all configurations found in configuration file \"{}\" in a user configuration file under {}".format("cov2seq.cfg", user_cfg))
 
